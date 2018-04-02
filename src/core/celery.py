ab3f85f5d2af371bedc31c814eb9b41fa4704af9
Kenan Bek <mail@kenanbek.me>
2018-04-03 01:43:39 +0300

    Additional celery configuration

--- src/core/celery.py
+++ src/core/celery.py
@@ -1,11 +1,14 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from django.conf import settings
 
-# set the default Django settings module for the 'celery' program.
+# Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-app = Celery('core')
+app = Celery('django-celery-skeleton')
+
+CELERY_TIMEZONE = 'UTC'
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
@@ -14,7 +17,7 @@ app = Celery('core')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
